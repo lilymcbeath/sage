c425a1f76f72f655e5dd6e59871ba63a085c59ae
Lily McBeath <51835421+lilymcbeath@users.noreply.github.com>
2024-02-09 13:08:45 -0800

    linear recurrences basic functionality

--- src/sage/combinat/all.py
+++ src/sage/combinat/all.py
@@ -276,6 +276,8 @@ lazy_import('sage.combinat.finite_state_machine_generators',
 # Sequences
 lazy_import('sage.combinat.binary_recurrence_sequences',
             'BinaryRecurrenceSequence')
+lazy_import('sage.combinat.linear_recurrence_sequences',
+            'LinearRecurrenceSequence')
 lazy_import('sage.combinat.recognizable_series', 'RecognizableSeriesSpace')
 lazy_import('sage.combinat.regular_sequence', 'RegularSequenceRing')
 
